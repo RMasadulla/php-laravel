734e69e7a778fe5c1bcf4f9b9f74ba45174a065b
Asadulla Ahmed <rma0011239@gmail.com>
2024-07-11 23:22:31 +0600

    added form validation info

--- app/Http/Controllers/personController.php
+++ app/Http/Controllers/personController.php
@@ -7,6 +7,38 @@ use Illuminate\Http\Request;
 class personController extends Controller
 {
     function addNewUser(Request $req) {
+
+        // $req->validate([
+        //     'name'=>'required | min:3 | max:10 ' ,
+        //     'email'=>'required | email',
+        //     'skill'=>'required',
+        //     'msg'=>'required | max:150 ',
+        // ]);
+
+
+        // for custom error message show
+
+        // $req->validate([
+        //     'name'=>'required | min:3 | max:10 ' ,
+        //     'email'=>'required | email',
+        //     'skill'=>'required',
+        //     'msg'=>'required | max:150 ',
+        // ], [
+        //     'name.required'=>'Enter your valid name',
+        //     'name.min'=>'You should be enter min char 3',
+
+        // ]);
+
+        // validation rule and custom error message showing
+        $req->validate([
+            'name'=>' uppercase ' ,
+        ], [
+            'name.uppercase'=> 'All char must be uppercase',
+        ]);
+        
         return $req;
+
+        // for array 
+        // print_r($req->skill);
     }
 }
--- resources/views/user/user-form.blade.php
+++ resources/views/user/user-form.blade.php
@@ -1,12 +1,48 @@
 <!-- @csrf need in form -->
 
+
+<!-- form error handle -->
+
+<!-- {{print_r($errors)}} -->
+
+<!-- @if($errors->any())
+    @foreach($errors->all() as $error)
+    <div>{{$error}}</div>
+    @endforeach
+@endif -->
+
+
 <div id="User">
     <h1>Add a new User</h1>
     <form action="add-new" method="post" class="userForm">
             @csrf
-        <input type="text" name="name" placeholder="Enter you name">
+        <input type="text" name="name" placeholder="Enter you name" value="{{old('name')}}"  class="{{ $errors->first('name') ? 'input-error' : '' }}">
+        <span style="color: red;">@error('name') {{$message}} @enderror</span>
+
         <input type="email" name="email" placeholder="Enter you valid email">
+        <span style="color: red;">@error("email") {{$message}} @enderror</span>
+
+        <div>
+            <h3>Skills</h3>
+            <span style="color: red;">@error("skill") {{$message}} @enderror</span>
+            <div id="checkField">
+                <div>
+                    <input type="checkbox" name="skill[]" value="php" id="php">
+                    <label for="php">PHP</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="skill[]" value="nodeJs" id="nodeJs">
+                    <label for="nodeJs">NodeJs</label>
+                </div>
+                <div>
+                    <input type="checkbox" name="skill[]" value="Html" id="Html">
+                    <label for="Html">Html</label>
+                </div>
+            </div>
+        </div>
         <textarea name="msg" placeholder="write something..."></textarea>
+        <span style="color: red;">@error("msg") {{$message}} @enderror</span>
+
         <button>Add New User</button>
     </form>
 </div>
@@ -24,7 +60,10 @@
         flex-direction: column;
         gap: 10px;
     }
-    
+    .input-error {
+        border: 1px solid red;
+        color: red;
+    }
     .userForm input {
         height: 40px;
     }
@@ -37,6 +76,18 @@
         font-weight: bold;
         padding: .6rem 0;
         border-radius: 8px;
+    }
+    .userForm #checkField {
+        display: flex;
+        align-items: center;
+        gap: 15px;
+    }
+    .userForm #checkField h3 {
+        margin: 0;
+    }
+    .userForm #checkField div {
+        display: flex;
+        align-items: center;
     }
     
 </style>
